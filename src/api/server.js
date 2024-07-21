c95d612e8f9eb776e1f3ae57db201368b97c4503
akid <141469567+akid3020@users.noreply.github.com>
2024-07-21 11:58:46 +0900

    fmx file path

--- src/api/server.js
+++ src/api/server.js
@@ -2,10 +2,10 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 
-// const router = jsonServer.router('db.json');
+// const router = jsonServer.router(path.join(process.cwd(), 'db.json'));
 const fs = require('fs');
 const path = require('path');
-const filePath = path.join('db.json');
+const filePath = path.join(process.cwd(), 'db.json');
 const data = fs.readFileSync(filePath, "utf-8");
 const db = JSON.parse(data);
 const router = jsonServer.router(db);
